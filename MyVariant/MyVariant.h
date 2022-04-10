ca171d6a97c374940543643c6affb0e85d389212
Владислав Балобанов <slava_balobanov@mail.ru>
2022-04-10 13:59:00 +0700

    Добавлены конструкторы для наборов из 1-го, 2-х, 3-х, 4-х типов.

--- MyVariant/MyVariant.h
+++ MyVariant/MyVariant.h
@@ -1,10 +1,18 @@
 #pragma once
 #include <iostream>
 
-template<typename I, typename B, typename F, typename D>
+template<typename T1, typename T2, typename T3, typename T4>
 class MyVariant
 {
 public:
+	MyVariant();
+	template<typename T1>
+	MyVariant();
+	template<typename T1, typename T2>
+	MyVariant();
+	template<typename T1, typename T2, typename T3>
+	MyVariant();
+	template<typename T1, typename T2, typename T3, typename T4>
 	MyVariant();
 	~MyVariant();
 };
