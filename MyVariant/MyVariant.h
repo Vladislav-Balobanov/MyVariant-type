d0d4ad6cc7944a63fa967cb39d6aaad38532facc
Владислав Балобанов <slava_balobanov@mail.ru>
2022-04-12 22:44:50 +0700

    Добавили новый конструктор и метод get().

--- MyVariant/MyVariant.h
+++ MyVariant/MyVariant.h
@@ -5,9 +5,13 @@ union MyVariant
 {
 public:
 	MyVariant();
+	template <typename type>
+	MyVariant(type value);
 	~MyVariant();
 	template <typename type>
 	void set(type value);
+	template <typename type>
+	type& get(type value);
 private:
 	int i_value;
 	bool b_value;
