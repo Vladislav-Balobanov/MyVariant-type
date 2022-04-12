7949dac5dd17c842ebd249caf0f508e191064bb4
Владислав Балобанов <slava_balobanov@mail.ru>
2022-04-12 22:33:56 +0700

    Реализация вариантного типа теперь есть реализация объединения (union).

--- MyVariant/MyVariant.h
+++ MyVariant/MyVariant.h
@@ -1,19 +1,7 @@
 #pragma once
 #include <iostream>
 
-template<typename T1, typename T2, typename T3, typename T4>
-class MyVariant
+union MyVariant 
 {
-public:
-	MyVariant();
-	template<typename T1>
-	MyVariant();
-	template<typename T1, typename T2>
-	MyVariant();
-	template<typename T1, typename T2, typename T3>
-	MyVariant();
-	template<typename T1, typename T2, typename T3, typename T4>
-	MyVariant();
-	~MyVariant();
-};
 
+};
